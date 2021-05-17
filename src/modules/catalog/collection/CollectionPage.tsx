1bf668c0eef5e53542e87d4db11839ca86acda66
Elizaveta <kovel.liza@mail.ru>
2021-05-17 13:15:47 +0300

    Added loader.

--- src/modules/catalog/collection/CollectionPage.tsx
+++ src/modules/catalog/collection/CollectionPage.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {startFetchData} from "../store/catalog.actions";
 import {RouteComponentProps, withRouter} from "react-router-dom";
+import {Loader} from "../../../components/Loader";
 interface RouterProps{
     path: string
 }
@@ -23,7 +24,7 @@ const CollectionPage: React.FC<CollectionProps> = ({history}) : JSX.Element =>{
                 title ? <h1>{title.toUpperCase()}</h1> : <h1>Not title</h1>
             }
             {
-                data.length == 0 ? <h1>Nothing</h1> : data.map(element => <h1 key={element.id}> {element.title}</h1>)
+                data.length == 0 ? <Loader/> : data.map(element => <h1 key={element.id}> {element.title}</h1>)
             }
     </div>
 )
