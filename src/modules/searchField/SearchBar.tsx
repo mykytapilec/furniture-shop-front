d988127ac8b038c236f2e21b0db562b9b3430623
Mikita Pilets <mykytapilec@gmail.com>
2021-05-19 11:36:40 +0300

    update search field

--- src/modules/searchField/SearchBar.tsx
+++ src/modules/searchField/SearchBar.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './searchField.css';
 
 
 export const SearchBar: React.FC = () => {
+    const [field, setField] = useState('');
+    const dispatch = useDispatch();
+    // const data = useSelector((state: Store) => state.catalogReducer.mainPageReducer);
+
+    useEffect(() => {
+        console.log(field)
+    }, []);
+
     return (
         <form className="search-field">
             <div className="input-field">
-                <input id="search" type="search" required />
-                <label className="label-icon"><i className="material-icons">search</i></label>
+                <input 
+                    id="search" 
+                    type="search" 
+                    value={field}
+                    onChange={e => setField(e.target.value)}
+                    required 
+                />
+                <label className="label-icon"><i className="material-icons">s</i></label>
                 <i className="material-icons">x</i>
             </div>
         </form>
